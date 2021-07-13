3667e3eb114f71023cf678271d0db3cff1db15f7
DrParadox05 <rksingh.rks6661@gmail.com>
2021-07-13 11:19:52 +0530

    templating engine set

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require("express");
+const mustExp = require("mustache-express");
 
 const app = express();
+const mustache = mustExp();
+mustache.cache = null;
+app.engine("mustache", mustache);
+app.set("view engine", "mustache");
+
+app.use(express.static("public"));
+
+app.get("/meds", (req, res) => {
+  res.render("meds");
+});
 
 app.listen(5555, () => {
   console.log("Listning to 5555");
